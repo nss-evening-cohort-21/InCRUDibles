2522d952f204c3119b9c966bad331af4b38d290f
ryanmbigelow <ryanmbigelow@gmail.com>
2023-02-01 21:33:22 -0600

    created profile element with minimal css design.

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,7 +1,21 @@
+import Image from 'next/image';
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../utils/context/authContext';
+import { signOut } from '../utils/auth';
 
-export default function boardData() {
+export default function UserProfile() {
+  const { user } = useAuth();
+
   return (
-    <div>boardData</div>
+    <div id="profileSection">
+      <Image src={user.photoURL} alt="userURL" width="200px" height="200px" />
+      <h2>{user.displayName}</h2>
+      <h5>{user.email}</h5>
+      <h5>Last Login: {user.metadata.lastSignInTime}</h5>
+      <Button type="button" class="btn btn-danger" size="lg" className="copy-btn" onClick={signOut}>
+        Sign Out
+      </Button>
+    </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   reactStrictMode: true,
   // I don't want it to run when compiling as I trust the CI stage of the pipeline and Husky.
   ignoreDuringBuilds: true,
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
 };
