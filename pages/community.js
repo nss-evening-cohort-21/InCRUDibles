8b20680ad93af465f1bbbea66959fe15e665e7f1
Wesley Vance <wslyvnce@gmail.com>
2023-02-07 19:54:05 -0600

    tested community page with api call

--- pages/community.js
+++ pages/community.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAllPubBoards } from '../api/boardData';
+import BoardCard from '../components/BoardCard';
 
-export default function boardData() {
+export default function Community() {
+  const [publicBoards, setPublicBoards] = useState([]);
+
+  const getAllPublicBoards = () => {
+    getAllPubBoards().then(setPublicBoards);
+  };
+
+  useEffect(() => {
+    getAllPublicBoards();
+  }, []);
   return (
-    <div>Community</div>
+    <div className="public-card-container">
+      {publicBoards.map((pubboard) => (
+        <BoardCard key={pubboard.firebaseKey} boardObj={pubboard} onUpdate={getAllPublicBoards} />
+      ))}
+    </div>
   );
 }
