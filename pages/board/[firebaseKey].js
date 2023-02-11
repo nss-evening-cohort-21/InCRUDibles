551edba5629f8a285cc24cce1dc3e120fab9bc45
Dro Demby <drotechdemby@gmail.com>
2023-02-11 10:06:09 -0600

    Fixed indentions

--- pages/board/[firebaseKey].js
+++ pages/board/[firebaseKey].js
@@ -32,7 +32,7 @@ export default function ViewBoard() {
         </div>
       </div>
       <div className="BD-pin-container">
-       <ViewYourPins />
+        <ViewYourPins />
       </div>
     </div>
   );
