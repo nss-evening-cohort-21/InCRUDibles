23bd9bab791772d1028ea86ac4d87239ad3498d1
Wesley Vance <wslyvnce@gmail.com>
2023-02-11 06:28:50 -0600

    board detail description now has board image as background

--- pages/board/[firebaseKey].js
+++ pages/board/[firebaseKey].js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { Button } from 'react-bootstrap';
@@ -24,7 +25,8 @@ export default function ViewBoard() {
   return (
 
     <div className="BD-container">
-      <div className="BD-detail-cont">
+      <div className="BD-detail-cont" style={{ backgroundImage: `url(${boardDetails.image})` }}>
+        {/* <div className="BD-photo-cont"><img className="BD-photo" src={boardDetails.image} alt={boardDetails.name} /></div> */}
         <div className="BD-name-cont"><h2 className="BD-name">{boardDetails.name}</h2></div>
         <div className="BD-desc-cont"><h5 className="BD-description">{boardDetails.description}</h5></div>
         <div className="BD-edit-cont">
