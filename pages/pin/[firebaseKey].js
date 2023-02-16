14bbbb3a529fdcd378c764bae7e0ddd531dd5c6d
Wesley Vance <wslyvnce@gmail.com>
2023-02-16 17:55:26 -0600

    button routes to board from pin page

--- pages/pin/[firebaseKey].js
+++ pages/pin/[firebaseKey].js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { viewPinDetails } from '../../api/mergedData';
+import { Button } from 'react-bootstrap';
+import Link from 'next/link';
 
 export default function ViewPin() {
   const [pinDetails, setPinDetails] = useState({});
@@ -33,6 +35,9 @@ export default function ViewPin() {
           <hr />
           <p className="PD-lock">
             {pinDetails.isPublic ? '🔓' : '🔒'}
+            <Link href={`/board/${pinDetails.board_id}`} passHref>
+              <Button variant="outline-dark" className="m-2">Return To Board</Button>
+            </Link>
           </p>
         </div>
       </div>
