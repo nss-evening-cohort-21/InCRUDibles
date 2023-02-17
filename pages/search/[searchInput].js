cfa851040fd54b279d3a2e1c1f2755238f794594
Wesley Vance <wslyvnce@gmail.com>
2023-02-16 18:08:42 -0600

    button

--- pages/search/[searchInput].js
+++ pages/search/[searchInput].js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { getPins } from '../../api/pinData';
